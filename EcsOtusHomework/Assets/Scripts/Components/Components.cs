6115012dcd1a1b7ed4e4b0350590178cc5a0edae
at-grigorievich-RUDN <at.grigorievich@gmail.com>
2025-03-25 15:59:57 +0300

    upd

--- EcsOtusHomework/Assets/Scripts/Components/Components.cs
+++ EcsOtusHomework/Assets/Scripts/Components/Components.cs
@@ -1,9 +1,20 @@
 ﻿using System;
+using Client.Data;
 using Leopotam.EcsLite.Entities;
 using UnityEngine;
 
 namespace Client.Components
 {
+    [Serializable]
+    public struct Team
+    {
+        public TeamType Value;
+    }
+    
+    
+    
+    
+    
     [Serializable]
     public struct Position
     {
---
+++ EcsOtusHomework/Assets/Scripts/Data/TeamType.cs
@@ -0,0 +1,8 @@
+﻿namespace Client.Data
+{
+    public enum TeamType: byte
+    {
+        Blue = 1,
+        Red = 2
+    }
+}
